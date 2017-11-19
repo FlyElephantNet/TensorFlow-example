3a3c14f285690e99b7ae2cb6c7ff374518ff9310
Dmitry Spodarets <d.spodarets@flyelephant.net>
2017-11-19 17:15:38 +0200

    Update keras_cifar10_v002_train_multigpu.py

--- keras_cifar10_v002_train_multigpu.py
+++ keras_cifar10_v002_train_multigpu.py
@@ -1,4 +1,3 @@
-
 # coding: utf-8
 from __future__ import absolute_import
 from __future__ import print_function
@@ -32,7 +31,7 @@ try:
     X_test = X_test.reshape(X_test.shape[0], X_test.shape[1]*X_test.shape[2]*X_test.shape[3])
 except Exception as err:
      print("exception ", format(err))
-               
+
 X_train = X_train.astype("float32")
 X_test = X_test.astype("float32")
 if (mean(X_train)>1):
@@ -42,32 +41,34 @@ print(X_train.shape[0], 'train samples')
 print(X_test.shape[0], 'test samples')
 
 # convert class vectors to binary class matrices
-batch_size = 128
+batch_size = 1024 # 128*8
 nb_classes = 10
 llrate= 3e-5
-nb_epoch = 100
+nb_epoch = 30
 regul= 1e-6
 Y_train = np_utils.to_categorical(y_train, nb_classes)
 Y_test = np_utils.to_categorical(y_test, nb_classes)
 do= 0.2
-model = Sequential()
-model.add(Dense(500,input_shape=(X_train.shape[1],)))
-#model.add(Dense(784, 128))
-model.add(Activation('tanh'))
-model.add(Dropout(do))
-model.add(Dense(256))
-model.add(Activation('relu'))
-model.add(Dense(64))
-model.add(Activation('relu'))
-model.add(Dropout(do))
-model.add(Dense(10))
-model.add(Activation('softmax'))
+
+with tf.device('/cpu:0'):
+	model = Sequential()
+	model.add(Dense(500,input_shape=(X_train.shape[1],)))
+	#model.add(Dense(784, 128))
+	model.add(Activation('tanh'))
+	model.add(Dropout(do))
+	model.add(Dense(256))
+	model.add(Activation('relu'))
+	model.add(Dense(64))
+	model.add(Activation('relu'))
+	model.add(Dropout(do))
+	model.add(Dense(10))
+	model.add(Activation('softmax'))
 
 rms= keras.optimizers.rmsprop(lr=llrate, decay=regul)
 adm= keras.optimizers.Adam(lr=llrate, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=regul)
-#model.compile(loss='mean_squared_error', optimizer=adm)
+model.compile(loss='mean_squared_error', optimizer=adm)
 parallel_model = multi_gpu_model(model, gpus=8)
-parallel_model.compile(loss='categorical_crossentropy', optimizer=adm)
+parallel_model.compile(loss='mean_squared_error', optimizer=adm)
 parallel_model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch, verbose=2, validation_data=(X_test, Y_test), callbacks=[tensorboard])
 score = model.evaluate(X_test, Y_test, verbose=1)
 print('Test score:', score)
